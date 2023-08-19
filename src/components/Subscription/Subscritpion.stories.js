d9195dbfc04b13918259b1ebb2fb90b55735925d
villecova <villecova@gmail.com>
2023-08-19 11:49:39 -0600

    feat(subscriptions): steries added

---
+++ src/components/Subscription/Subscritpion.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Primary } from "../Button/Button.stories";
+import { Primary } from "../Button/Button.stories";
+import { Large } from "../Input/Input.stories";
+
+export default {
+    title: "Form/Subsription"
+}
+
+export const PrimarySubscription = () => (
+    <>
+        <Large/>
+        <Primary/>
+    </>
+)
